7573a228de42673dea9ec3fc3df057d46281d62b
RoboTums <t.rack721@gmail.com>
2021-08-19 14:38:57 -0400

    added caching. do use_cache=1

--- query_dns.py
+++ query_dns.py
@@ -51,13 +51,22 @@ def get_public_dns_servers(dns_server_website="https://dnschecker.org/public-dns
     #clean DNS_Server_table. Just some regex lamdba on an IP address, amazon interview question
     dns_server_table['IP Address'] = dns_server_table['IP Address'].apply(lambda x: parse_IP(x))
     return dns_server_table
+def read_cache():
+    return pd.read_csv('cache.csv',index_col=0)
 
 if __name__ == '__main__':
-    dns_server_table = get_public_dns_servers()
     parser = argparse.ArgumentParser(description='Checks ~300 DNS servers to find the CDN provider for a specified URL endpoint.')
     parser.add_argument('--url', type=str, default="images-na.ssl-images-amazon.com", help="enter a url that needs a CDN to be served")
+    parser.add_argument('--use_cache', type=int, default=1, help="1 or 0 that determines whether you use cache.csv")
     parser.add_argument('--output_file', type=str, default="us_aws_cdn.csv", help="string that ends in .csv for the output data.")
+    parser.add_argument("--company",type=str, default="AMZN", help="generally what company the url serves")
     args = parser.parse_args()
+
+    if args.use_cache == 1:
+        dns_server_table = read_cache()
+    else:
+        dns_server_table = get_public_dns_servers()
+
     today = date.today().strftime("%m/%d/%Y")
     cdn_providers = {}
     ip_data = {}
@@ -65,7 +74,7 @@ if __name__ == '__main__':
         cmd = f"dig @{ip_address.split()[0]} {args.url}"
         #print(cmd)
         proc=subprocess.Popen(shlex.split(cmd),stdout=subprocess.PIPE,
-    stderr=subprocess.DEVNULL)
+            stderr=subprocess.DEVNULL)
         out,err=proc.communicate()
         string_output = out.decode('utf-8')
         string_output_split = string_output.split('ANSWER SECTION')
@@ -81,13 +90,13 @@ if __name__ == '__main__':
                     'usage':1,
                     'date':today,
                      }
-                ip_data[ip_address]={
-                        'location':current_series['Location'].iloc[0].split('\n')[0],
-                        'reliability':current_series['Reliability'].iloc[0].split('\n')[0],
-                        'CDN':filtered_str,
-                        'date':today
-                        }
-                
+            ip_data[ip_address]={
+                    'location':current_series['Location'].iloc[0].split('\n')[0],
+                    'reliability':current_series['Reliability'].iloc[0].split('\n')[0],
+                    'CDN':filtered_str,
+                    'date':today
+                    }
+            
 
     cdn_provider_DF = pd.DataFrame(cdn_providers)
     cdn_provider_DF.to_csv(date.today().strftime("%m_%d_%Y")+"_"+args.output_file)
